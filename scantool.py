40b0586839d8ca6a3f802e585bb6098ad36ca739
Pess1 <Pess1>
2021-02-11 12:50:01 +0200

    Comments to scantool

--- scantool.py
+++ scantool.py
@@ -6,7 +6,7 @@ import re
 def main():
 	serverIps = ['0.0.0.0']
 	userName = 'uname'
-
+	#This could later be used if Ip's were retrieved from an API
 	for serverIp in serverIps:
 
 		ssh = subprocess.Popen(['ssh', '-t', userName + '@' + serverIp, "dpkg -l | grep -E 'mariadb|postgresql|nodejs'",],
@@ -14,19 +14,26 @@ def main():
 			stdout=subprocess.PIPE,
 			stderr=subprocess.PIPE)
 		results = ssh.stdout.readlines()
-
+		# If result array is empty it is a falsy value
 		if not results:
 			error = ssh.stderr.readlines()
 			print('ERROR: %s' % error, sys.stderr)
 		else:
+			#List to save the dicts containing dependency name and version
 			outputArr = []
+			#The before mentioned dict
 			dict = {"depName": "", "depVer": ""}
+			#Looping through the lines retrieved by the command
+			#TODO: Create a function out of the formatter
 			for result in results:
+				#Select only interesting part of the string and remove leading and trailing whitespace
 				formattedResult = str(result)[6:72].strip()
-				objectData = re.sub("\s+", "|", formattedResult).split("|")
+				#Add the depname and version to dict
+				objectData = re.split("\s+", formattedResult)
 				dict["depName"] = objectData[0]
 				dict["depVer"] = objectData[1]
 				outputArr.append(dict)
+			#Creating the final output with serverIp and depInfo
 			serverDict = {"serverIp": serverIp, "depInfo": outputArr}
 			print(serverDict)
 main()
